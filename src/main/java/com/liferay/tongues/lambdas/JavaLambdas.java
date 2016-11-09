868fa5ed73f0a4bd615e7ba4e21b66a8622ac512
Vishal Reddy <vishal.reddy@liferay.com>
2016-11-09 14:53:22 -0800

    Java First Class Lambdas

---
+++ src/main/java/com/liferay/tongues/lambdas/JavaLambdas.java
@@ -0,0 +1,7 @@
+package com.liferay.tongues.lambdas;
+
+import java.util.function.BiFunction;
+
+public class JavaLambdas {
+	public static BiFunction<Integer, Integer, Integer> addThis = (a, b) -> a + b;
+}
---
+++ src/test/java/com/liferay/tongues/lambdas/JavaLambdasTests.java
@@ -0,0 +1,15 @@
+package com.liferay.tongues.lambdas;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JavaLambdasTests {
+
+	@Test
+	public void addThisTest() {
+		Assert.assertEquals("Java lambdas don't really work",
+			JavaLambdas.addThis.apply(Integer.valueOf(1), Integer.valueOf(1)),
+			Integer.valueOf(2));
+	}
+
+}
